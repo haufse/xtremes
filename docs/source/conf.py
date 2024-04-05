e934d13347701498e8395ab93b8b5e79df94263c
Erik Haufs <88788235+haufse@users.noreply.github.com>
2024-04-05 10:00:21 +0200

    Update conf.py template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'python_docs_theme'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'python_docs_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
