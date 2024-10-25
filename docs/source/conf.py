cb510b50fd2a8caad106e8ee122f5647db502eb1
haufse <erik@haufs.de>
2024-10-25 10:31:22 +0200

    center fig

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,6 +87,7 @@ latex_elements = {
     {\large Author: Erik Haufs \par}
     \vfill
     \begin{figure}[!htp]
+    \centering
     \includegraphics[width=0.4\textwidth]{../../docs/source/images/logo4.png}\par
     \caption{Dall-E's interpretation of the Xtremes package}
     \end{figure}
